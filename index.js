1f0919b8d380480928b6111fe0233daa8090511f
swills044 <swills044@gmail.com>
2021-11-27 18:38:31 +0000

    new

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-const { ocrSpace } = require('ocr-space-api-wrapper');
+const { ocrSpace } = require('ocr-space-api-wrapper'); //https://www.npmjs.com/package/ocr-space-api-wrapper
 const express = require('express')
 const sqlite3 = require('sqlite3').verbose();
-const app = express()
+const app = express();
+const cors = require("cors");
+var bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
 const port = 3000
+
+app.use(cors())
+
+app.use(express.static(__dirname + '/public'));
 
 //What happens when someone sends a requests to the program. does it start a new program everytime?
 
@@ -11,22 +24,82 @@ app.get('/', (req, res) => {
   main().then((res)=>{console.log(res)});
 })
 
+app.post('/sendText', (req, res) => {
+  console.log(req.body);
+  extractDetails(req.body);
+  res.send('Hello World!');
+})
+
+function extractDetails(text) {
+
+  //There should be 5 lines with an additional line for random code at the top of the label
+
+  //Two codes, store both in main style code and secondary style code (put longest style code in main)
+
+  //Two texts the main text (item name) and secondary text (style colour) *The name of the colour matters because they will be seperate entries (keep aware of same colour names with different wording, may be useful to store colour code), put the first one in main text (item name)
+
+  //Price will always have £ symbol
+
+  //The size will either be xs, s, m, xl, xxl (2xl), xxxl (3xl), xxxxl (4xl), 5xl -> IGNORE size for now
+
+  //The barcode will always be 12 or 13 characters (should be 13)
+
+}
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
-async function main () {
-  try {
+// async function main () {
+//   try {
 
-    // Using your personal API key + local file
-    const res2 = await ocrSpace('http://dl.a9t9.com/ocrbenchmark/eng.png', { apiKey: '83e4ad974c88957' });
+//     // Using your personal API key + local file
+//     const res2 = await ocrSpace('http://dl.a9t9.com/ocrbenchmark/eng.png', { apiKey: '83e4ad974c88957' });
     
-    return res2;
-  } catch (error) {
-    console.error(error);
+//     return res2;
+//   } catch (error) { 
+//     console.error(error);
+//   }
+// }
+
+let db = new sqlite3.Database('stock.db', sqlite3.OPEN_READWRITE, (err) => {
+  if (err) {
+    return console.error(err.message);
   }
+  console.log('Connected to the SQlite database.');
+});
+
+
+var sqlDROPTABLE = `DROP TABLE contacts;`
+var sqlCREATETABLE = `CREATE TABLE IF NOT EXISTS items (
+  item_id INTEGER PRIMARY KEY,
+  item_code TEXT,
+  item_name TEXT,
+  item_colour TEXT,
+  item_size TEXT,
+  item_barcode TEXT
+);
+`
+sqlINSERT = `INSERT INTO items (item_code, item_name, item_name, item_colour, item_size, item_barcode)
+VALUES (value1,value2 ,...)`
+
+function sqlRUN(query) {
+  db.all(query,[],(err, rows ) => {
+    // process rows here 
+    if (err) {
+      throw err;
+    }
+    console.log(rows);
+    rows.forEach((row) => {
+      console.log(row.name);
+    });   
+  });
+  
 }
 
+//sqlRUN(sqlCREATETABLE);
+
+/*
 let db = new sqlite3.Database('stock.db', sqlite3.OPEN_READWRITE, (err) => {
   if (err) {
     return console.error(err.message);
@@ -37,18 +110,6 @@ let db = new sqlite3.Database('stock.db', sqlite3.OPEN_READWRITE, (err) => {
 var sqlINSERT = `INSERT INTO stock (contact_id, first_name, last_name, email, phone)VALUES(1,'sean', 'a', 'b', 'c')`
 var sqlREAD = `SELECT 'contact_id', 'first_name' FROM contacts;`
 
-db.all(sqlINSERT,[],(err, rows ) => {
-  // process rows here 
-  if (err) {
-    throw err;
-  }
-  console.log(rows);
-  rows.forEach((row) => {
-    console.log(row.name);
-  });   
-});
-
-
 // close the database connection
 db.close((err) => {
   if (err) {
@@ -57,26 +118,8 @@ db.close((err) => {
   console.log('Close the database connection.');
 });
 
-//Create DB
-
-// CREATE TABLE contacts (
-// 	contact_id INTEGER PRIMARY KEY,
-// 	first_name TEXT NOT NULL,
-// 	last_name TEXT NOT NULL,
-// 	email TEXT NOT NULL UNIQUE,
-// 	phone TEXT NOT NULL UNIQUE
-// );
-
-//Insert DB
-
-// INSERT INTO table1 (column1,column2 ,..)
-// VALUES 
-//    (value1,value2 ,...),
-//    (value1,value2 ,...),
-//     ...
-//    (value1,value2 ,...);
-
 //Find rows DB
 
 // SELECT column_list
 // FROM table;
+*/
